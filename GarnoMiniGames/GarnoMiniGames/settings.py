746a8891731396f3b3e8f1febff52736d4fdaa42
Brian-Belleau <brianbelleau10@gmail.com>
2026-09-03 18:21:17 -0400

    Correction hebergement mdp

--- GarnoMiniGames/GarnoMiniGames/settings.py
+++ GarnoMiniGames/GarnoMiniGames/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'GarnoMiniGames.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'garnominigames_dev',
-        'USER': 'postgres',
-        'PASSWORD': 'alloallo',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('PGDATABASE', 'garnominigames_dev'),
+        'USER': os.getenv('PGUSER', 'postgres'),
+        'PASSWORD': os.getenv('PGPASSWORD', ''),
+        'HOST': os.getenv('PGHOST', 'localhost'),
+        'PORT': os.getenv('PGPORT', '5432'),
     }
 }
 
